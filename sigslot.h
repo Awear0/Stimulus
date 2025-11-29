d7dcdd4bd5316bf681f04cfbaaac7ca8ffe02ec7
Armand Rossius <>
2025-11-29 17:41:07 +0100

    Added connection handling

--- sigslot.h
+++ sigslot.h
@@ -3,6 +3,7 @@
 
 #include <concepts>
 #include <functional>
+#include <memory>
 #include <type_traits>
 #include <utility>
 #include <vector>
@@ -13,9 +14,13 @@ concept not_void = (!std::same_as<NotVoid, void>);
 template<class SignalArgs>
 concept signal_arg = (not_void<SignalArgs> && !std::is_rvalue_reference_v<SignalArgs>);
 
+class connection;
+
 class emitter
 {
 public:
+    friend class connection;
+
     virtual ~emitter() = default;
 
 protected:
@@ -29,20 +34,121 @@ protected:
     {
         (self.*emitted_signal).emit(std::forward<EmittedArgs>(emitted_args)...);
     }
+
+private:
+    class connection_holder;
+};
+
+class emitter::connection_holder
+{
+public:
+    virtual ~connection_holder() = default;
+
+    virtual void disconnect() = 0;
+    virtual void suspend() = 0;
+    virtual void resume() = 0;
+};
+
+class connection
+{
+public:
+    connection(std::weak_ptr<emitter::connection_holder> weak_holder):
+        m_holder { std::move(weak_holder) }
+    {
+    }
+
+    connection(const connection&) = default;
+    connection(connection&&) = default;
+    auto operator=(const connection&) -> connection& = default;
+    auto operator=(connection&&) -> connection& = default;
+
+    void disconnect()
+    {
+        auto locked_holder { m_holder.lock() };
+        if (!locked_holder)
+        {
+            return;
+        }
+
+        locked_holder->disconnect();
+    }
+
+    void suspend()
+    {
+        auto locked_holder { m_holder.lock() };
+        if (!locked_holder)
+        {
+            return;
+        }
+
+        locked_holder->suspend();
+    }
+
+    void resume()
+    {
+        auto locked_holder { m_holder.lock() };
+        if (!locked_holder)
+        {
+            return;
+        }
+
+        locked_holder->resume();
+    }
+
+private:
+    std::weak_ptr<emitter::connection_holder> m_holder;
+};
+
+class scoped_connection
+{
+public:
+    scoped_connection(connection conn):
+        m_connection { std::move(conn) }
+    {
+    }
+
+    ~scoped_connection()
+    {
+        disconnect();
+    }
+
+    void disconnect()
+    {
+        m_connection.disconnect();
+    }
+
+private:
+    connection m_connection;
+};
+
+class inhibitor
+{
+public:
+    inhibitor(connection conn):
+        m_connection { std::move(conn) }
+    {
+        m_connection.suspend();
+    }
+
+    ~inhibitor()
+    {
+        m_connection.resume();
+    }
+
+private:
+    connection m_connection;
 };
 
 template<signal_arg... Args>
 class emitter::signal final
+
 {
 public:
     friend emitter;
     using slot = std::function<void(Args...)>;
 
     template<std::invocable<Args...> Callable>
-    void connect(Callable&& callable) const
-    {
-        m_slots.emplace_back(std::forward<Callable>(callable));
-    }
+    connection connect(Callable&& callable) const;
 
 private:
     template<std::convertible_to<Args>... EmittedArgs>
@@ -56,15 +162,79 @@ private:
         auto begin { m_slots.begin() };
         auto previous_to_end { std::prev(m_slots.end()) };
 
+        // TODO AROSS: Handle exceptions
+
         for (auto it { begin }; it != previous_to_end; ++it)
         {
-            (*it)(emitted_args...);
+            (**it)(emitted_args...);
         }
 
-        m_slots.back()(std::forward<EmittedArgs>(emitted_args)...);
+        (*m_slots.back())(std::forward<EmittedArgs>(emitted_args)...);
     }
 
-    mutable std::vector<slot> m_slots {};
+    class connection_holder_implementation;
+
+    void disconnect(connection_holder_implementation* holder) const
+    {
+        std::erase_if(m_slots, [holder](const auto& slot) { return slot.get() == holder; });
+    }
+
+    mutable std::vector<std::shared_ptr<connection_holder_implementation>> m_slots {};
 };
+
+// TODO AROSS: operator= strategy for signal
+template<signal_arg... Args>
+class emitter::signal<Args...>::connection_holder_implementation final: public connection_holder
+{
+public:
+    template<std::convertible_to<signal::slot> Callable>
+    connection_holder_implementation(const signal& connected_signal, Callable&& callable):
+        m_signal { connected_signal },
+        m_slot { std::forward<Callable>(callable) }
+    {
+    }
+
+    template<class... EmittedArgs>
+        requires std::invocable<signal::slot, EmittedArgs...>
+    void operator()(EmittedArgs&&... args)
+    {
+        if (m_suspended)
+        {
+            return;
+        }
+        m_slot(std::forward<EmittedArgs>(args)...);
+    }
+
+    void disconnect() override
+    {
+        m_signal.disconnect(this);
+    }
+
+    void suspend() override
+    {
+        m_suspended = true;
+    }
+
+    void resume() override
+    {
+        m_suspended = false;
+    }
+
+private:
+    signal::slot m_slot;
+    const signal& m_signal;
+    bool m_suspended { false };
+};
+
+template<signal_arg... Args>
+template<std::invocable<Args...> Callable>
+connection emitter::signal<Args...>::connect(Callable&& callable) const
+{
+    m_slots.emplace_back(
+        std::make_shared<connection_holder_implementation>(*this,
+                                                           std::forward<Callable>(callable)));
+
+    return { m_slots.back() };
+}
 
 #endif // SIGSLOT_H_
---
+++ tests/test_connection.cpp
@@ -0,0 +1,147 @@
+#include "../sigslot.h"
+
+#include <gtest/gtest.h>
+
+#include "utilities.h"
+
+class test_connection: public ::testing::Test
+{
+protected:
+    generic_emitter<> empty_emitter;
+};
+
+TEST_F(test_connection, basic_disconnection)
+{
+    int& count = call_count<>;
+    reset<>();
+
+    auto connection { empty_emitter.generic_signal.connect(slot_function<>) };
+    EXPECT_EQ(count, 0);
+
+    empty_emitter.generic_emit();
+    EXPECT_EQ(count, 1);
+
+    connection.disconnect();
+
+    empty_emitter.generic_emit();
+    EXPECT_EQ(count, 1);
+}
+
+TEST_F(test_connection, doesnt_disconnect_when_going_out_of_scope)
+{
+    int& count = call_count<>;
+    reset<>();
+
+    {
+        auto connection { empty_emitter.generic_signal.connect(slot_function<>) };
+        EXPECT_EQ(count, 0);
+
+        empty_emitter.generic_emit();
+        EXPECT_EQ(count, 1);
+    }
+
+    empty_emitter.generic_emit();
+    EXPECT_EQ(count, 2);
+}
+
+TEST_F(test_connection, scoped_connection)
+{
+    int& count = call_count<>;
+    reset<>();
+
+    {
+        scoped_connection scoped_connection { empty_emitter.generic_signal.connect(
+            slot_function<>) };
+        EXPECT_EQ(count, 0);
+
+        empty_emitter.generic_emit();
+        EXPECT_EQ(count, 1);
+    }
+
+    empty_emitter.generic_emit();
+    EXPECT_EQ(count, 1);
+}
+
+TEST_F(test_connection, scoped_connection_manual_disconnection)
+{
+    int& count = call_count<>;
+    reset<>();
+
+    {
+        scoped_connection scoped_connection { empty_emitter.generic_signal.connect(
+            slot_function<>) };
+        EXPECT_EQ(count, 0);
+
+        scoped_connection.disconnect();
+
+        empty_emitter.generic_emit();
+        EXPECT_EQ(count, 0);
+    }
+
+    empty_emitter.generic_emit();
+    EXPECT_EQ(count, 0);
+}
+
+TEST_F(test_connection, basic_suspend_resume)
+{
+    int& count = call_count<>;
+    reset<>();
+
+    auto connection { empty_emitter.generic_signal.connect(slot_function<>) };
+    EXPECT_EQ(count, 0);
+
+    empty_emitter.generic_emit();
+    EXPECT_EQ(count, 1);
+
+    connection.suspend();
+
+    empty_emitter.generic_emit();
+    EXPECT_EQ(count, 1);
+
+    connection.resume();
+
+    empty_emitter.generic_emit();
+    EXPECT_EQ(count, 2);
+}
+
+TEST_F(test_connection, doesnt_resume_when_going_out_of_scope)
+{
+    int& count = call_count<>;
+    reset<>();
+
+    {
+        auto connection { empty_emitter.generic_signal.connect(slot_function<>) };
+        EXPECT_EQ(count, 0);
+
+        empty_emitter.generic_emit();
+        EXPECT_EQ(count, 1);
+
+        connection.suspend();
+
+        empty_emitter.generic_emit();
+        EXPECT_EQ(count, 1);
+    }
+
+    empty_emitter.generic_emit();
+    EXPECT_EQ(count, 1);
+}
+
+TEST_F(test_connection, basic_inhibitor)
+{
+    int& count = call_count<>;
+    reset<>();
+
+    {
+        inhibitor connection { empty_emitter.generic_signal.connect(slot_function<>) };
+        EXPECT_EQ(count, 0);
+
+        empty_emitter.generic_emit();
+        EXPECT_EQ(count, 0);
+
+        empty_emitter.generic_emit();
+        EXPECT_EQ(count, 0);
+    }
+
+    empty_emitter.generic_emit();
+    EXPECT_EQ(count, 1);
+}
