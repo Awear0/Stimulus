95f06f92df55caeab1db2b24bcc8dd75f49dcda8
Armand Rossius <>
2025-11-30 23:33:42 +0100

    Added tests for all_different concept

---
+++ tests/test_all_different.cpp
@@ -0,0 +1,20 @@
+#include "../sigslot.h"
+
+#include <gtest/gtest.h>
+
+using ::testing::Test;
+
+TEST(all_different, basic_test)
+{
+    EXPECT_EQ((all_different<0, 1, 2, 3, 4, 5>), true);
+    EXPECT_EQ((all_different<0, 1, 2>), true);
+    EXPECT_EQ((all_different<0, 1>), true);
+    EXPECT_EQ((all_different<0, 4>), true);
+    EXPECT_EQ((all_different<0, 4, 1>), true);
+    EXPECT_EQ((all_different<0>), true);
+    EXPECT_EQ((all_different<1>), true);
+    EXPECT_EQ((all_different<>), true);
+    EXPECT_EQ((all_different<1, 1>), false);
+    EXPECT_EQ((all_different<1, 0, 1>), false);
+    EXPECT_EQ((all_different<1, 0, 2, 3, 4, 5, 0>), false);
+}
